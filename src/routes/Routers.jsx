2481ce4ee88af0bdb8fc1622f831d1116b39651e
ShailySarker <shailysarker99@gmail.com>
2025-03-11 22:57:51 +0600

    initial setup completed

---
+++ src/routes/Routers.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router";
+import LandingPage from "../pages/LandingPage/LandingPage";
+import Home from "../pages/Home/Home";
+import MainLayouts from "../layouts/MainLayouts";
+
+const Routers = () => {
+    return (
+        <Routes>
+            <Route
+                index
+                element={<LandingPage />}
+            />
+            <Route
+                path="/"
+                element={<MainLayouts />}
+            >
+                <Route
+                    path="/home"
+                    element={<Home />}
+                />
+            </Route>
+        </Routes >
+    );
+};
+
+export default Routers;

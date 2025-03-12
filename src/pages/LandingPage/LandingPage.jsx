07c9efda491776dd4683e7b51a007360c58adf25
ShailySarker <shailysarker99@gmail.com>
2025-03-12 22:34:38 +0600

    LandingPage added

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FaStethoscope } from 'react-icons/fa';
+import { GiStethoscope } from 'react-icons/gi';
+import { PiStethoscopeDuotone } from 'react-icons/pi';
+import "./LandingPage.css";
+import { useNavigate } from 'react-router';
 
 const LandingPage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/home");
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
     return (
-        <div>
-            LandingPage
+        <div className="fw-bolder d-flex flex-row justify-content-center align-items-center text-primary vh-100 vw-100 gap-2 gap-md-3 pulse">
+            <GiStethoscope className="display-4" />
+            <p className="h1">Medi Care</p>
         </div>
     );
 };
